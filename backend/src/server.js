ade8327b85f16671707f7d1238085162f9dc13ea
omkar454 <omraut41105@gmail.com>
2025-10-30 19:55:49 +0530

    Serve React build from backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,7 +29,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../public", "index.html"));
 });
 
